d3b89c34775610e84009aafc2818a8d92f7c41ff
LegenD742 <devansh7420@gmail.com>
2026-03-19 23:53:26 +0530

    trailing slash

--- client/src/services/snippetApi.js
+++ client/src/services/snippetApi.js
@@ -4,7 +4,7 @@ const API = axios.create({
   baseURL: "/api/snippets"
 });
 
-export const getSnippets = () => API.get("/");
-export const createSnippet = (data) => API.post("/", data);
+export const getSnippets = () => API.get("");
+export const createSnippet = (data) => API.post("", data);
 export const updateSnippet = (id, data) => API.put(`/${id}`, data);
 export const deleteSnippet = (id) => API.delete(`/${id}`);
